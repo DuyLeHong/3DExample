6ad4fe9cd9239f80ee7d291b0157f8b79d0b6df9
Duy Le <lehongduy.bk@gmail.com>
2023-11-20 19:06:11 +0700

    Initial commit

---
+++ Assets/Scripts/tuongtac.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class tuongtac : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Lấy giá trị đầu vào từ bàn phím
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Tính toán hướng di chuyển
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Tính toán vị trí mới
+        Vector3 newPosition = transform.position + movement * speed * Time.deltaTime;
+
+        // Di chuyển đối tượng đến vị trí mới
+        transform.position = newPosition;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "cay")
+        {
+            Debug.Log("Va cham cay");
+        } else if (other.tag == "nui")
+        {
+            Debug.Log("Va cham nui");
+        } else if (other.tag == "dat")
+        {
+            Debug.Log("Va cham dat");
+        } else
+        {
+            Debug.Log("va cham");
+        }
+    }
+}
